11b90d0e1ad9db70c33e5d6eb9aabee993e9a2fe
pedrogones <pedrotargin@gmail.com>
2024-08-20 16:40:24 -0300

    add load spinner element

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
 import { SharedMessagesService } from './../shared-messages/shared-messages.service';
 import { Component } from '@angular/core';
 import { FiltersComponent } from "../filters/filters.component";
+import { SpinnerComponent } from '../layouts/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FiltersComponent],
+  imports: [FiltersComponent, SpinnerComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  isLoading: boolean = false;
+
   constructor(private sharedMessage: SharedMessagesService){
 
   }
 
-  seeDetails(){
-      this.sharedMessage.errorMessageSwal("Método não implementado");
-  }
+  seeDetails() {
+    this.isLoading = true;
 
+    setTimeout(() => {
+      this.sharedMessage.errorMessageSwal("Método não implementado");
+      this.isLoading = false;
+    }, 2000);
+  }
 }
--- src/app/layouts/render/render.component.ts
+++ src/app/layouts/render/render.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { SidebarComponent } from '../side-bar/sidebar/sidebar.component';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-render',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, SidebarComponent],
+  imports: [RouterOutlet, NavBarComponent, SidebarComponent, SpinnerComponent, CommonModule],
   templateUrl: './render.component.html',
   styleUrl: './render.component.scss'
 })
 export class RenderComponent {
+isLoading: any;
 
 }
