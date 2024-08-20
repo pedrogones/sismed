f6474f7619c739c2a9db1b3b6344b36a45821f82
pedrogones <pedrotargin@gmail.com>
2024-08-20 16:57:33 -0300

    add spinner usage examples

--- src/app/medicos/medicos.component.ts
+++ src/app/medicos/medicos.component.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
 import { SharedMessagesService } from '../shared-messages/shared-messages.service';
+import { SpinnerComponent } from '../layouts/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-medicos',
   standalone: true,
-  imports: [],
+  imports: [SpinnerComponent, CommonModule],
   templateUrl: './medicos.component.html',
   styleUrl: './medicos.component.scss'
 })
 export class MedicosComponent {
+  isLoading: boolean = false;
   constructor(private sharedMessage: SharedMessagesService){
 
   }
 
   search(){
-    this.sharedMessage.warningMessageSwal("Método de busca não implementado");
+    this.isLoading = true;
+    setTimeout(() => {
+      this.sharedMessage.warningMessageSwal("Método de busca não implementado");
+      this.isLoading = false;
+    }, 2000);
   }
   remove(){
+    this.isLoading = true;
+    setTimeout(() => {
       this.sharedMessage.warningMessageSwal("Método delete não implementado");
+      this.isLoading = false;
+    }, 2000);
   }
   update(){
+    this.isLoading = true;
+    setTimeout(() => {
     this.sharedMessage.warningMessageSwal("Método update não implementado");
+    this.isLoading = false;
+  }, 2000);
   }
 }
