33e933ee4f913220820e9c2ae712ae98ec638609
pedrogones <pedrotargin@gmail.com>
2024-08-25 18:06:23 -0300

    configuring HttpClient providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layouts/side-bar/sidebar/sidebar.component';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SidebarComponent],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
